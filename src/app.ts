66dcb876ff5df5043679a9e896c1ee6e8e3b78b6
angjan <janusz.angerman@gmail.com>
2025-04-13 21:49:03 +0200

    add logging

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
 import express from "express";
+import fs from "fs";
+import morgan from "morgan";
+import path from "path";
 import itemRoutes from "./routes/itemRoutes";
+
 import { errorHandler } from "./middlewares/errorHandler";
-
 const app = express();
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "../", "logs", "access.log"),
+  {
+    flags: "a",
+  },
+);
 
+// setup the logger
 app.use(express.json());
+app.use(morgan("dev", { stream: accessLogStream }));
 app.use(errorHandler);
 
 app.use("/items", itemRoutes);
