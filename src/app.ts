369a6e135487abef1c9ce756307f908c1b8addcf
angjan <janusz.angerman@gmail.com>
2025-04-14 21:28:42 +0200

    changed logging level, added helmet

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import fs from "fs";
+import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import itemRoutes from "./routes/itemRoutes";
 
 import { errorHandler } from "./middlewares/errorHandler";
 const app = express();
+
 // create a write stream (in append mode)
 const accessLogStream = fs.createWriteStream(
   path.join(__dirname, "../", "logs", "access.log"),
@@ -15,8 +17,9 @@ const accessLogStream = fs.createWriteStream(
 );
 
 // setup the logger
+app.use(helmet());
 app.use(express.json());
-app.use(morgan("dev", { stream: accessLogStream }));
+app.use(morgan("combined", { stream: accessLogStream }));
 app.use(errorHandler);
 
 app.use("/items", itemRoutes);
